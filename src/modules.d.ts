0ef537d392f64b78a88b8a858dd429a5b09b7310
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2023-05-30 22:56:30 -0500

    new release export global css

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,7 @@
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
