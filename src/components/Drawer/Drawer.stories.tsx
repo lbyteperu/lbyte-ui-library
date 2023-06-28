06dda2ecfd3ce38d176d8e27c7498eefd2c1279c
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2023-06-27 20:35:10 -0500

    add drawer

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,9 +1,8 @@
+import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { withRouter } from "storybook-addon-react-router-v6";
-import Drawer from "./Drawer";
-
-import React from "react";
-
+import Drawer, { DrawerProps } from "./Drawer";
+import Button from "../Button";
 type Story = StoryObj<typeof Drawer>;
 
 /**
@@ -15,6 +14,62 @@ export default {
   tags: ["autodocs"],
 } satisfies Meta<typeof Drawer>;
 
-export const Default: Story = {
+export const Basic: Story = {
+  parameters: {
+    open: false,
+  },
   args: {},
+  render: () => <Drawer open={true} />,
+};
+/**
+ * Examples with state
+ */
+const DrawerWithState = ({ appearFrom }: DrawerProps) => {
+  const [open, setOpen] = useState(true);
+  const handleOnChange = () => {};
+  return (
+    <div
+      style={{ display: "flex", flexFlow: "row", justifyContent: "flex-end" }}
+    >
+      <Drawer
+        open={open}
+        mountOnEnter
+        unmountOnExit
+        setOpen={setOpen}
+        appearFrom={appearFrom}
+      />
+      <Button
+        onClick={() => {
+          setOpen(false);
+        }}
+        color="primary"
+      >
+        cerrar
+      </Button>
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+        color="secondary"
+      >
+        abrir
+      </Button>
+    </div>
+  );
+};
+
+export const FromLeft: Story = {
+  render: () => <DrawerWithState appearFrom="left" open={true} />,
+};
+
+export const FromRight: Story = {
+  render: () => <DrawerWithState appearFrom="right" open={true} />,
+};
+
+export const FromAbove: Story = {
+  render: () => <DrawerWithState appearFrom="above" open={true} />,
+};
+
+export const FromBelow: Story = {
+  render: () => <DrawerWithState appearFrom="below" open={true} />,
 };
--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,35 +1,108 @@
 import classList from "classnames";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import { createPortal } from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import styles from "./Drawer.module.css";
-import Button from "../Button";
-import Content from "./Content"
+import Content from "./Content";
+import { CSSTransitionClassNames } from "react-transition-group/CSSTransition";
+import IconButton from "../IconButton/IconButton";
+import { AddCircle } from "../Icons/AddCircle";
 
-
-const Drawer = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  return (
-    <div>
-      <CSSTransition
-        in={isOpen}
-        timeout={300}
-        classNames={{...styles}}
-        unmountOnExit
-      >
-        <>
-          {isOpen &&
-            createPortal(<Content onClose={setIsOpen} />, document.body)}
-        </>
-      </CSSTransition>
-      <Button
-        color="primary"
-        onClick={() => setIsOpen((prevState) => !prevState)}
-      >
-        test
-      </Button>
-    </div>
-  );
+export type DrawerProps = {
+  children?: React.ReactNode;
+  open: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  unmountOnExit?: boolean;
+  mountOnEnter?: boolean;
+  portalContainer?: Element | DocumentFragment;
+  onClosed?: () => void;
+  appearFrom?: "left" | "right" | "above" | "below";
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+const TrasitionOptions: Record<string, CSSTransitionClassNames> = {
+  left: {
+    enter: styles.fromLeftEnter,
+    enterActive: styles.fromLeftEnterActive,
+    enterDone: styles.fromLeftEnterDone,
+    exit: styles.fromLeftExit,
+    exitActive: styles.fromLeftExitActive,
+    exitDone: styles.fromLeftExitDone,
+  },
+  right: {
+    enter: styles.fromRightEnter,
+    enterActive: styles.fromRightEnterActive,
+    enterDone: styles.fromRightEnterDone,
+    exit: styles.fromRightExit,
+    exitActive: styles.fromRightExitActive,
+    exitDone: styles.fromRightExitDone,
+  },
+  above: {
+    enter: styles.fromAboveEnter,
+    enterActive: styles.fromAboveEnterActive,
+    enterDone: styles.fromAboveEnterDone,
+    exit: styles.fromAboveExit,
+    exitActive: styles.fromAboveExitActive,
+    exitDone: styles.fromAboveExitDone,
+  },
+  below: {
+    enter: styles.fromBelowEnter,
+    enterActive: styles.fromBelowEnterActive,
+    enterDone: styles.fromBelowEnterDone,
+    exit: styles.fromBelowExit,
+    exitActive: styles.fromBelowExitActive,
+    exitDone: styles.fromBelowExitDone,
+  },
+};
+
+function Drawer({
+  children,
+  open,
+  unmountOnExit,
+  mountOnEnter,
+  setOpen,
+  portalContainer = document.body,
+  appearFrom = "left",
+}: DrawerProps) {
+  const nodeRef = useRef(null);
+
+  const { fromLeft, fromRight, fromAbove, fromBelow } = styles;
+  const mainCss =
+    appearFrom === "left"
+      ? fromLeft
+      : appearFrom === "right"
+      ? fromRight
+      : appearFrom === "above"
+      ? fromAbove
+      : appearFrom === "below"
+      ? fromBelow
+      : "left";
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={open}
+      nodeRef={nodeRef}
+      timeout={300}
+      classNames={TrasitionOptions[appearFrom]}
+      unmountOnExit={unmountOnExit}
+      mountOnEnter={mountOnEnter}
+      onEnter={() => console.log("on enter")}
+      onExited={() => console.log("on exited")}
+    >
+      <div ref={nodeRef} className={mainCss}>
+        <IconButton
+          color="primary"
+          icon={<AddCircle size="small" />}
+          size="small"
+          onClick={() => setOpen && setOpen(false)}
+        />
+        <p>This alert message is being transitioned in and out of the DOM.</p>
+      </div>
+    </CSSTransition>,
+    portalContainer
+  );
+}
 
 export default Drawer;
