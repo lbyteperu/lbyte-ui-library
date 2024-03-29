d5f7842918cb0c8ece94bc3b809e4526bf9376b5
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2024-03-29 08:48:17 -0500

    add scss

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 // components/index.ts
-export * from "./button"; // Add more exports for other components as needed
+export * from "./Button"; // Add more exports for other components as needed
+export * from "./TestComponent"; // Add more exports for other components as needed
